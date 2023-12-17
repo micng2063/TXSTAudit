4706c1c665a9306f266b12a896dd56b88bbcb7c9
Michelle Nguyen <rnb90@txstate.edu>
2023-12-16 18:16:32 -0600

    Change each course from <td> to <button> for clicking

--- client/src/routes/Course.js
+++ client/src/routes/Course.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import "../css/Course.css";
+import { FaCheck } from "react-icons/fa";
 
 function removeParentheses(courseCode) {
   const regex = /\([^)]*\)/g;
@@ -9,40 +10,74 @@ function removeParentheses(courseCode) {
 
 function Course() {
   const [courseData, setCourseData] = useState([]);
+  const [fallCheckColor, setFallCheckColor] = useState([]);
+  const [springCheckColor, setSpringCheckColor] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:5050/scrape')
       .then((response) => {
         setCourseData(response.data);
+        setFallCheckColor(Array(response.data.length).fill("#747474"));
+        setSpringCheckColor(Array(response.data.length).fill("#747474"));
       })
       .catch((error) => {
         console.error('Error fetching course data', error);
       });
   }, []);
 
+  const handleButtonClick = (index, semester) => {
+    if (semester === 'fall') {
+      setFallCheckColor((prev) => {
+        const newState = [...prev];
+        newState[index] = newState[index] === "#747474" ? "#50C878" : "#747474";
+        return newState;
+      });
+    } else if (semester === 'spring') {
+      setSpringCheckColor((prev) => {
+        const newState = [...prev];
+        newState[index] = newState[index] === "#747474" ? "#50C878" : "#747474";
+        return newState;
+      });
+    }
+  };
+
   return (
     <div className="course-content">
       <h1>Course Requirements</h1>
-      <table style={{marginLeft:"250px"}}>
-        <thead>
-          <tr>
-            <th style={{paddingLeft:'10px'}}><b>Fall Semester Course</b></th>
-            <th style={{paddingRight:"50px"}}><b>Hours</b></th>
-            <th style={{paddingLeft:'10px'}}><b>Spring Semester Course</b></th>
-            <th style={{paddingRight:"50px"}}><b>Hours</b></th>
-          </tr>
-        </thead>
-        <tbody>
-          {courseData.map(({ fallSemester, springSemester }, index) => (
-            <tr key={index}>
-              <td dangerouslySetInnerHTML={{ __html: removeParentheses(fallSemester.courseCode) }} style={{paddingLeft:'10px'}}/>
-              <td>{fallSemester.hours}</td>
-              <td dangerouslySetInnerHTML={{ __html: removeParentheses(springSemester.courseCode) }}  style={{paddingLeft:'10px'}}/>
-              <td>{springSemester.hours}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {courseData.map(({ fallSemester, springSemester }, index) => (
+          <div className="grid-course" key={index} style={{ marginBottom: "10px" }}>
+            <div className="item">
+              {fallSemester.courseCode.trim() && ( 
+                <button 
+                  className="grid-course-button" 
+                  onClick={() => handleButtonClick(index, 'fall')}
+                >
+                  <span style={{ color: fallCheckColor[index]}}>
+                    <strong>{removeParentheses(fallSemester.courseCode)}</strong>
+                  </span>
+                  <span style={{color:"#747474", paddingLeft:"10px"}}>Course description</span>
+                  <FaCheck style={{ float: "right", paddingRight: '10px', color: fallCheckColor[index] }} />
+                </button>
+              )}
+            </div>
+            <div className="item">
+              {springSemester.courseCode.trim() && ( 
+                <button 
+                  className="grid-course-button" 
+                  onClick={() => handleButtonClick(index, 'spring')}
+                >
+                  <span style={{ color: springCheckColor[index]}}>
+                    <strong>{removeParentheses(springSemester.courseCode)}</strong>
+                  </span>
+                  <span style={{color:"#747474", paddingLeft:"10px"}}>Course description</span>
+                  <FaCheck style={{ float: "right", paddingRight: '10px', color: springCheckColor[index] }} />
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
