507c33f9b2314c667496f8978a29692eea071ecb
Michelle Nguyen <rnb90@txstate.edu>
2023-12-19 10:13:06 -0600

    Implemented basic interface of button

--- client/src/routes/AddCourse.js
+++ client/src/routes/AddCourse.js
@@ -1,7 +1,14 @@
-function AddCourse(){
-    return(
-        <div>
-            <button>Add course</button>
+import { FaPlus } from "react-icons/fa";
+import "../css/Course.css";
+
+function AddCourse() {
+    return (
+        <div className="course-content">
+            <div className="grid-course" style={{ marginTop: "10px", marginLeft: "21.5%" }}>
+                <button className="grid-course-button"><FaPlus style={{ paddingRight: "20px", color: "#747474" }} />Add course</button>
+            </div>
         </div>
     );
 }
+
+export default AddCourse;
