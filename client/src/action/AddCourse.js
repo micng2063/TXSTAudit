c5942523b61e9105c877ed96993c1665b421096e
Michelle Nguyen <rnb90@txstate.edu>
2023-12-29 10:04:40 -0600

    Modified form to notify whether course exists or not

--- client/src/action/AddCourse.js
+++ client/src/action/AddCourse.js
@@ -11,7 +11,7 @@ function AddCourse() {
   const [showForm, setShowForm] = useState(false);
   const [courseName, setCourseName] = useState("");
   const [courseDetails, setCourseDetails] = useState([]);
-  const [courseNotFound, setCourseNotFound] = useState({});
+  const [courseNotFound, setCourseNotFound] = useState(false);
 
   useEffect(() => {
     setShowForm(false);
@@ -19,7 +19,7 @@ function AddCourse() {
 
   const handleAddClick = () => {
     setShowForm(true);
-    setCourseNotFound({});
+    setCourseNotFound(false);
   };
 
   const handleFormSubmit = async (e) => {
@@ -30,7 +30,7 @@ function AddCourse() {
         const data = await response.json();
 
         if (data && data.length === 0) {
-          setCourseNotFound((prevNotFound) => ({ ...prevNotFound, [courseName]: true }));
+          setCourseNotFound(true);
         } else {
           if (!courseDetails.some(detail => detail.courseName === courseName)) {
             setCourseDetails((prevDetails) => [...prevDetails, { courseName, details: data }]);
@@ -50,7 +50,7 @@ function AddCourse() {
       const data = await response.json();
 
       if (data && data.length === 0) {
-        setCourseNotFound((prevNotFound) => ({ ...prevNotFound, [courseName]: true }));
+        setCourseNotFound(true);
       } else {
         if (!courseDetails.some(detail => detail.courseName === courseName)) {
           setCourseDetails((prevDetails) => [...prevDetails, { courseName, details: data }]);
@@ -80,8 +80,7 @@ function AddCourse() {
           index % 2 === 0 && (
             <button key={index} className="grid-course-button" style={{ marginTop: "10px" }}>
               <span style={{ color: "#5aac44" }}><strong>{detail.courseName}</strong></span>
-              <span style={{ color: "#747474", paddingLeft: "10px" }}>
-                {courseNotFound[detail.courseName] ? 'Course not found in catalog' : (detail.details.length > 0 ? detail.details[0].CourseName : 'Course description')}
+              <span style={{ color: "#747474", paddingLeft: "10px" }}>{(detail.details.length > 0 ? detail.details[0].CourseName : 'Course description')}
               </span>
               <FaTimes onClick={() => handleRemoveCourse(index)} style={{ float: "right", paddingRight: "20px", marginTop: "5px", color: "#e9e9e9" }} />
             </button>
@@ -90,34 +89,37 @@ function AddCourse() {
       </div>
       <div className="grid-course" style={{ marginLeft: "14%" }}>
         {showForm ? (
-          <button className="grid-course-button" style={{ marginTop: "10px" }}>
-            <form onSubmit={handleFormSubmit}>
-              <div className="grid-add-course">
-                <div className="item" style={{ paddingTop: "5px" }}>
-                  <TextField
-                    label="Course"
-                    variant="outlined"
-                    value={courseName}
-                    onChange={(e) => setCourseName(e.target.value)}
-                    placeholder="Enter course name"
-                    style={{ height: "50px", border: "1px solid #fff", width: "250px" }}
-                    inputProps={{ style: { backgroundColor: "#fff", color: "#747474", fontFamily: 'Segoe UI', fontSize: "17px" }, notchedOutline: { borderColor: "#747474" } }}
-                    InputLabelProps={{ style: { fontFamily: "Segoe UI", color: "#747474" } }}
-                  />
-                </div>
-                <div className="item" style={{ paddingTop: "10px", paddingLeft: "5px" }}>
-                  <button type="submit" variant="contained" onClick={handleSearch}
-                    style={{ width: "100px", height: "40px", backgroundColor: "#5aac44", border: "1px #5aac44", color: "#fff", borderRadius: "5px" }}>
-                    <FaPlus style={{ paddingRight: "10px", color: "#fff" }} />  <strong>Add</strong>
-                  </button>
-                </div>
+          <button className="grid-course-button" style={{ marginTop: "10px", height: courseNotFound ? "110px" : "80px" }}>
+          <form onSubmit={handleFormSubmit}>
+            <div className="grid-add-course">
+              <div className="item" style={{ paddingTop: "5px" }}>
+                <TextField
+                  label="Course"
+                  variant="outlined"
+                  value={courseName}
+                  onChange={(e) => setCourseName(e.target.value)}
+                  placeholder="Enter course name"
+                  style={{ height: "50px", border: "1px solid #fff", width: "250px" }}
+                  inputProps={{ style: { backgroundColor: "#fff", color: "#747474", fontFamily: 'Segoe UI', fontSize: "17px" }, notchedOutline: { borderColor: "#747474" } }}
+                  InputLabelProps={{ style: { fontFamily: "Segoe UI", color: "#747474" } }}
+                />
               </div>
-            </form>
-          </button>
+              <div className="item" style={{ paddingTop: "10px", paddingLeft: "5px" }}>
+                <button type="submit" variant="contained" onClick={handleSearch}
+                  style={{ width: "100px", height: "40px", backgroundColor: "#5aac44", border: "1px #5aac44", color: "#fff", borderRadius: "5px" }}>
+                  <FaPlus style={{ paddingRight: "10px", color: "#fff" }} /><strong>Add</strong>
+                </button>
+              </div>
+            </div>
+          </form>
+          {courseNotFound && (
+            <div style={{ color: "#747474", paddingLeft:"10px" }}><i>Course not found, please try again.</i></div>
+          )}
+        </button>
+        
         ) : (
           <button className="grid-course-button" style={{ marginTop: "10px" }} onClick={handleAddClick}>
-            <FaPlus style={{ paddingRight: "20px", color: "#747474" }} />
-            Add course
+            <FaPlus style={{ paddingRight: "20px", color: "#747474" }} /> Add course
           </button>
         )}
       </div>
